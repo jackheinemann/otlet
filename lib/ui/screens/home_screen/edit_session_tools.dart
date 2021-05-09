44df488118435d0f8a48eb764056f77e3e07ce61
Jack Heinemann <jack@oenoke.com>
2021-05-09 16:27:11 -0400

    indicate no active session tools on session start

--- lib/ui/screens/home_screen/edit_session_tools.dart
+++ lib/ui/screens/home_screen/edit_session_tools.dart
@@ -22,30 +22,37 @@ class _EditSessionToolsState extends State<EditSessionTools> {
         valueControllers =
             activeSessionTools.map((e) => TextEditingController()).toList();
       return Expanded(
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text('Session Tools',
-                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500)),
-            ),
-            Expanded(
-                child: ListView.builder(
-                    itemCount: activeSessionTools.length,
-                    itemBuilder: (context, i) {
-                      Tool tool = activeSessionTools[i];
-                      return tool.generateValueInput(
-                          context, valueControllers[i], labelText: tool.name,
-                          onValueChange: (value) {
-                        setState(() {
-                          tool.value = value;
-                          valueControllers[i].text = tool.displayValue();
-                          instance.updateSessionTool(tool);
-                        });
-                      });
-                    })),
-          ],
-        ),
+        child: activeSessionTools.isEmpty
+            ? Center(
+                child: Text('No Active Session Tools',
+                    style:
+                        TextStyle(fontSize: 18, fontWeight: FontWeight.w400)),
+              )
+            : Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text('Session Tools',
+                        style: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.w500)),
+                  ),
+                  Expanded(
+                      child: ListView.builder(
+                          itemCount: activeSessionTools.length,
+                          itemBuilder: (context, i) {
+                            Tool tool = activeSessionTools[i];
+                            return tool.generateValueInput(
+                                context, valueControllers[i],
+                                labelText: tool.name, onValueChange: (value) {
+                              setState(() {
+                                tool.value = value;
+                                valueControllers[i].text = tool.displayValue();
+                                instance.updateSessionTool(tool);
+                              });
+                            });
+                          })),
+                ],
+              ),
       );
     });
   }
