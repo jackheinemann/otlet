047afeab314550b9c7866b72e6cb7d2440175e2f
Jack Heinemann <jack@oenoke.com>
2021-05-31 22:03:41 -0400

    cleaned up launch aesthetic

--- lib/ui/delegates/nav_manager.dart
+++ lib/ui/delegates/nav_manager.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:otlet/business_logic/models/otlet_instance.dart';
 import 'package:otlet/business_logic/services/local_storage_manager.dart';
+import 'package:otlet/business_logic/utils/constants.dart';
 import 'package:otlet/ui/delegates/tab_manager.dart';
 
 class NavManager extends StatelessWidget {
@@ -15,13 +16,21 @@ class NavManager extends StatelessWidget {
             return TabManager(otletInstance);
           }
           return Scaffold(
-            appBar: AppBar(title: Icon(Icons.menu_book), centerTitle: true),
+            // appBar: AppBar(centerTitle: true),
+            // body: Center(
+            //   child: Container(
+            //     width: MediaQuery.of(context).size.width * .5,
+            //     child: LinearProgressIndicator(
+            //       backgroundColor: Theme.of(context).backgroundColor,
+            //       valueColor: AlwaysStoppedAnimation<Color>(
+            //           Theme.of(context).accentColor),
+            //     ),
+            //   ),
+            // ),
+            backgroundColor: primaryColor,
             body: Center(
-              child: LinearProgressIndicator(
-                backgroundColor: Theme.of(context).backgroundColor,
-                valueColor: AlwaysStoppedAnimation<Color>(
-                    Theme.of(context).accentColor),
-              ),
+              // color: primaryColor,
+              child: Image.asset('assets/launcher/book_logo_launcher.png'),
             ),
           );
         });
